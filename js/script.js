df30f8a2fc4a653515e8bee95e5659f3afb54be5
Mahmud <rainbowmahmud@gmail.com>
2022-02-17 08:20:41 +0600

    Code is finished with bonus section

--- js/script.js
+++ js/script.js
@@ -1,57 +1,20 @@
-// Error check
-function createNode(e) {
-  e.target.focus();
-  e.target.style.border = "2px solid red";
-  // e.target.style.color = "red";
+let initial = {
+  balance: 0,
+  expenses: 0,
+};
 
-  if (e.target.parentElement.childElementCount < 2) {
-    let errorNode = document.createElement("small");
-    errorNode.setAttribute("class", "text-danger error-msg");
-    e.target.parentNode.appendChild(errorNode);
-    return errorNode;
-  } else {
-    return document.querySelector("#" + e.target.id + " + small");
-  }
-}
-
-document.querySelector(".income-saving-container").addEventListener(
-  "blur",
-  function (e) {
-    e.stopImmediatePropagation();
-
-    if (e.target.className.includes("form-control")) {
-      const { value } = e.target;
-
-      switch (true) {
-        case !Boolean(value):
-          createNode(e).innerHTML = "Can not be empty";
-          break;
-
-        case isNaN(value):
-          createNode(e).innerHTML = "Only number is allowed";
-          break;
-
-        case Number(value) < 1:
-          createNode(e).innerHTML = "Only positive value. Please";
-          break;
-
-        default:
-          if (e.target.parentNode.childElementCount > 1) {
-            document.querySelector("#" + e.target.id + " + small").remove();
-          }
-          e.target.style.border = "2px solid green";
-      }
-    }
-  },
-  true
-);
+//  ========== Error check ======
+// Checking validity of input element
+document.getElementById("income").focus();
+document
+  .querySelector(".income-saving-container")
+  .addEventListener("blur", validateInput, true);
 
 // ======== EventListener CLICK ===========
-
 // Calculate button is clicked
 document
   .getElementById("calculate-income")
-  .addEventListener("click", function () {
+  .addEventListener("click", function (e) {
     // Calculate Expenses
     const expense = calculateExpenses(
       getValue("food"),
@@ -59,47 +22,54 @@ document
       getValue("clothes")
     );
 
-    // show in element
-    setValue("total-expenses", formatToReadableDigit(expense), "innerText");
+    // check amount to save
+    if (isAmountAvailable(e, Number(getValue("income")), expense)) {
+      // set total expenses
+      initial.expenses = expense;
+      setValue("total-expenses", formatToReadableDigit(expense), "innerText");
 
-    // Update Balance
-    const balance = calculateBalance(getValue("income"), expense);
-    setValue("balance", balance, "innerText");
-
-    // clear input field
-    // setValue("food");
-    // setValue("rent");
-    // setValue("clothes");
-    // setValue("income");
+      // Update Balance
+      const balance = calculateBalance(getValue("income"), expense);
+      initial.balance = balance;
+      setValue("balance", formatToReadableDigit(initial.balance), "innerText");
+    }
   });
 
 // Saving Button Clicked
 document
   .getElementById("calculate-saving")
-  .addEventListener("click", function () {
+  .addEventListener("click", function (e) {
     // get saving percentage
     const income = getValue("income");
-    const percent = parseInt(getValue("saving-percent")) / 100;
+    const percent = parseInt(Number(getValue("saving-percent"))) / 100;
     const savingAmount = Number(income) * percent;
 
-    // set saving amount
-    setValue("saving-amount", formatToReadableDigit(savingAmount), "innerText");
+    // check amount to save
+    if (isAmountAvailable(e, Number(initial.balance), savingAmount)) {
+      // set saving amount
+      setValue(
+        "saving-amount",
+        formatToReadableDigit(savingAmount),
+        "innerText"
+      );
 
-    // update balance
-    const totalCost =
-      Number(getValue("total-expenses", "innerText").slice(1)) + savingAmount;
-    const remainingBalance = calculateBalance(income, totalCost);
+      // update balance
+      const totalCost = Number(initial.expenses) + savingAmount;
+      const remainingBalance = calculateBalance(
+        Number(initial.balance),
+        totalCost
+      );
 
-    // Set remaining balance
-    setValue(
-      "remaining-balance",
-      formatToReadableDigit(remainingBalance),
-      "innerText"
-    );
+      // Set remaining balance
+      setValue(
+        "remaining-balance",
+        formatToReadableDigit(remainingBalance),
+        "innerText"
+      );
+    }
   });
 
 // ============ Functions ==========
-
 // Calculate Expenses
 function calculateExpenses() {
   let total = 0;
@@ -115,11 +85,71 @@ function calculateExpenses() {
 function calculateBalance(income, expense) {
   const remaining = Number(income) - Number(expense);
 
-  return formatToReadableDigit(remaining);
+  return remaining;
+}
+
+// isAmountAvailable - check amount to save
+function isAmountAvailable(e, principal, cost) {
+  let errorNode = document.querySelector("#" + e.target.id + " ~ .error-text");
+
+  if (principal > cost) {
+    errorNode.innerText = "";
+    return true;
+  } else {
+    errorNode.innerText = `Amount, ${formatToReadableDigit(
+      principal
+    )} is low to Cost, ${formatToReadableDigit(cost)}`;
+  }
+
+  return false;
+}
+
+// Validate Input
+function validateInput(e) {
+  e.stopImmediatePropagation();
+
+  if (e.target.className.includes("form-control")) {
+    const { value } = e.target;
+
+    switch (true) {
+      case !Boolean(value):
+        createNode(e).innerHTML = "Can not be empty";
+        break;
+
+      case isNaN(value):
+        createNode(e).innerHTML = "Only number is allowed";
+        break;
+
+      case Number(value) < 1:
+        createNode(e).innerHTML = "Only positive value. Please";
+        break;
+
+      default:
+        if (e.target.parentNode.childElementCount > 1) {
+          document.querySelector("#" + e.target.id + " + small").remove();
+        }
+        e.target.style.border = "2px solid green";
+    }
+  }
+}
+
+// Creating error msg node or updating text content
+function createNode(e) {
+  e.target.focus();
+  e.target.style.border = "2px solid red";
+
+  if (e.target.parentElement.childElementCount < 2) {
+    let errorNode = document.createElement("small");
+    errorNode.setAttribute("class", "text-danger error-msg");
+    e.target.parentNode.appendChild(errorNode);
+
+    return errorNode;
+  } else {
+    return document.querySelector("#" + e.target.id + " + small");
+  }
 }
 
 // =========== UTILITIES Functions =============
-
 // 1. get value of dom
 function getValue(id, action = "value") {
   return document.getElementById(id)[action];
@@ -131,7 +161,7 @@ function setValue(id, value = "", action = "value") {
 }
 
 // 3. Number formatting to readable digit
-function formatToReadableDigit(number, currency = "EUR") {
+function formatToReadableDigit(number, currency = "USD") {
   const formattedNumber = number.toLocaleString(undefined, {
     style: "currency",
     currency: currency,
